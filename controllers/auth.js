777aeda6949776570b362a4e48416f7f31c109c4
andres97473 <andres97473@hotmail.com>
2022-03-22 08:15:56 -0500

    commit 2022-03-22 08:15

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,7 @@ const bcrypt = require("bcryptjs");
 const Usuario = require("../models/usuario");
 const { generarJWT } = require("../helpers/jwt");
 const { googleVerify } = require("../helpers/google-verify");
+const { getMenuFrontEnd } = require("../helpers/menu-frontend");
 
 const login = async (req, res = response) => {
   const { email, password } = req.body;
@@ -32,6 +33,7 @@ const login = async (req, res = response) => {
     res.json({
       ok: true,
       token,
+      menu: getMenuFrontEnd(usuarioDB.role),
     });
   } catch (error) {
     console.log(error);
@@ -73,8 +75,8 @@ const googleSignIn = async (req, res = response) => {
 
     res.json({
       ok: true,
-      msg: "Google signin",
       token,
+      menu: getMenuFrontEnd(usuario.role),
     });
   } catch (error) {
     res.status(401).json({
@@ -95,6 +97,7 @@ const renewToken = async (req, res = response) => {
     ok: true,
     token,
     usuario,
+    menu: getMenuFrontEnd(usuario.role),
   });
 };
 
