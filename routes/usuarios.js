cf0ee5e9cd15aba2c5483d41746921e31a6e45c7
andres97473 <andres97473@hotmail.com>
2022-03-26 12:11:54 -0500

    Fin de seccion 18

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -5,7 +5,11 @@
 const { Router } = require("express");
 const { check } = require("express-validator");
 const { validarCampos } = require("../middlewares/validar-campos");
-const { validarJWT, validarADMIN_ROLE } = require("../middlewares/validar-jwt");
+const {
+  validarJWT,
+  validarADMIN_ROLE,
+  validarADMIN_ROLE_o_MismoUsuario,
+} = require("../middlewares/validar-jwt");
 const {
   getUsuarios,
   crearUsuarios,
@@ -31,7 +35,7 @@ router.put(
   "/:id",
   [
     validarJWT,
-    validarADMIN_ROLE,
+    validarADMIN_ROLE_o_MismoUsuario,
     check("nombre", "El nombre es obligatorio").not().isEmpty(),
     check("email", "El email es obligatorio").isEmail(),
     check("role", "El role es obligatorio").not().isEmpty(),
